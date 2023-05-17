6e31979264e55b6ca12051b5e59063adee821d34
Jawad Ali <jawad.ali@repairdesk.co>
2023-05-17 17:24:09 +0500

    animate with auto animate

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
+<script setup>
+import { ref } from "vue"
+
+const numbers = ref( new Array( 50 ).fill( "" ).map( ( _, i ) => i ) )
+const randomize = () => {
+  numbers.value.sort( () => ( Math.random() > 0.5 ? 1 : -1 ) )
+}
+</script>
+
 <template>
-  <div>
-    <nav class="bg-gray-800 p-4">
-      <router-link class="text-white" to="/">Home</router-link>
-      <router-link class="text-white" to="/about">About</router-link>
-    </nav>
-    <router-view v-slot="{ Component }">
-      <!-- <transition name="fade" mode="out-in"> -->
-      <transition mode="out-in" enter-active-class="animate__animated animate__fadeInLeft"
-        leave-active-class="animate__animated animate__fadeOutLeft">
-        <component :is="Component" />
-      </transition>
-    </router-view>
+  <div class="example boxes-example">
+    <div class="flex flex-wrap m-2" v-auto-animate="{ duration: 500 }">
+      <div class="w-[calc(10% - 0.5em)] m-[0.25em] flex items-center justify-center border" v-for="number in numbers"
+        :key="number" v-text="number" />
+    </div>
+    <button class="button button--alt" @click="randomize">Randomize</button>
   </div>
 </template>
 
-<script setup>
-// import { RouterView } from "vue-router"
-</script>
+<style scoped>
+.boxes {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2em -0.25em 2em -0.25em;
+}
+
+.box {
+  box-sizing: border-box;
+  width: calc(10% - 0.5em);
+  margin: 0.25em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.875em;
+  font-weight: 300;
+  aspect-ratio: 1;
+  border: 1px solid var(--gray-l);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "./index.scss"
 import router from "./router"
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+
 
 const app = createApp( App )
 app.use( createPinia() )
+app.use( autoAnimatePlugin )
 app.use( router )
 app.mount( '#rd-appstore' )
